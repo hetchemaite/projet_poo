91161526d118d3079a032326726bc3b732dfdd7e
Laurent Reveillere <laurent.reveillere@u-bordeaux.fr>
2020-11-08 17:03:03 +0100

    comment

--- src/main/java/fr/ubx/poo/model/decor/Decor.java
+++ src/main/java/fr/ubx/poo/model/decor/Decor.java
@@ -6,6 +6,9 @@ package fr.ubx.poo.model.decor;
 
 import fr.ubx.poo.model.Entity;
 
+/***
+ * A decor is an element that does not know its own position in the grid.
+ */
 public class Decor extends Entity {
 
 }
--- src/main/java/fr/ubx/poo/model/go/GameObject.java
+++ src/main/java/fr/ubx/poo/model/go/GameObject.java
@@ -8,6 +8,9 @@ import fr.ubx.poo.game.Position;
 import fr.ubx.poo.game.Game;
 import fr.ubx.poo.model.Entity;
 
+/***
+ * A GameObject can acces the game and knows its position in the grid.
+ */
 public abstract class GameObject extends Entity {
     protected final Game game;
     private Position position;
